5a70e7bf9e61455207ebdfa4bd8c7a4493e206f7
AryanRajJha <113089226+AryanRajJha@users.noreply.github.com>
2026-06-14 21:38:21 +0530

    Update DriverFactory.java

--- Wipro_capstone_project/GUIElementsAutomation/src/main/java/utils/DriverFactory.java
+++ Wipro_capstone_project/GUIElementsAutomation/src/main/java/utils/DriverFactory.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,11 +16,11 @@ public class DriverFactory {
 
         ChromeOptions options = new ChromeOptions();
 
-options.addArguments("--headless=new");
-options.addArguments("--disable-gpu");
-options.addArguments("--window-size=1920,1080");
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
 
-driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
